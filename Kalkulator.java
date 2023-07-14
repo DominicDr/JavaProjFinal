43c4a0b9a24ae1d886a76e148fcd1795d120888b
Dominik Mateusz Drążek <ddrazek@artemis.wszib.edu.pl>
2023-07-14 21:48:15 +0200

    Dodanie dodatkowej operacji pierwiastkowania drugiego i trzeciego stopnia

--- Kalkulator.java
+++ Kalkulator.java
@@ -14,7 +14,7 @@ public class Kalkulator {
         
         System.out.println("Dostępne operacje:");
 
-        System.out.println(" 1. Dodawanie\n 2. Odejmowanie\n 3. Mnożenie\n 4. Dzielenie\n 5. Potęgowanie\n 6. Procent z liczby");
+        System.out.println(" 1. Dodawanie\n 2. Odejmowanie\n 3. Mnożenie\n 4. Dzielenie\n 5. Potęgowanie\n 6. Procent z liczby\n 7. Pierwiastkowanie 2 lub 3 stopnia");
        
         
         System.out.print("Wybierz numer operacji która ma zostać wykonana na liczbach " + liczba1 + " oraz " + liczba2 + ":");
@@ -51,9 +51,24 @@ public class Kalkulator {
                 wynik = (liczba1 * liczba2) / 100;
                 System.out.println(liczba2 + "% liczby " + liczba1 + " wynosi: " + wynik);
                 break;
-            default:
-                System.out.println("Nieprawidłowy numer operacji!");
-                break;
+	    case 7:
+		if (liczba2==2) {
+			wynik = Math.sqrt(liczba1);
+			System.out.println("Pierwiastek kwadratowy z liczby " + liczba1 + " wynosi: " + wynik);
+			}
+		
+		else if (liczba2==3) {
+			wynik = Math.cbrt(liczba1);
+			System.out.println("Pierwiastek sześcienny z liczby " + liczba1 + " wynosi: " + wynik);
+			}
+
+		else {
+			System.out.println("Nieprawidłowy stopień pierwiastka!");
+			}
+		break;
+	   default:
+		System.out.println("Nieprawidłowy numer operacji!");
+		break;
         }
     }
 }
